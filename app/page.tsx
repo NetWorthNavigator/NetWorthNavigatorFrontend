15693b08e3d93394b7e065694fd5d65d0251f838
Alex Keith <alex.michael.keith@gmail.com>
2024-06-09 15:23:00 -0400

    update test

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { API_URL } from "../constants";
 
 
 export default function Home() {
-  console.log('API_URL:', API_URL);
-  console.log('NEXT_PUBLIC_API_URL_PROD:', process.env.NEXT_PUBLIC_API_URL_PROD);
-  console.log('NEXT_PUBLIC_API_URL_DEV:', process.env.NEXT_PUBLIC_API_URL_DEV);
   return (
     <main className="flex h-screen w-screen">
       <Header />
