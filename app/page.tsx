6e8d54720a1f791d616c59df983108f87c241b11
Alex Keith <alex.michael.keith@gmail.com>
2024-06-09 11:58:23 -0400

    test url

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Header from "../components/header/Header";
-
+import { API_URL } from "../constants";
 export default function Home() {
   return (
     <main className="flex h-screen w-screen">
       <Header />
+      {API_URL}
     </main>
   );
 }
