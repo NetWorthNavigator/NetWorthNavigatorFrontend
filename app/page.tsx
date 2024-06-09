0ce6265088e7eceadc3f3d1d6b390273df32a55d
Alex Keith <alex.michael.keith@gmail.com>
2024-06-09 14:38:13 -0400

    test env

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "../components/header/Header";
 import { API_URL } from "../constants";
+
+
 export default function Home() {
   console.log('API_URL:', API_URL);
   console.log('NEXT_PUBLIC_API_URL_PROD:', process.env.NEXT_PUBLIC_API_URL_PROD);
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,8 +1,16 @@
-export default function portfolio() {
+export default function Portfolio({ apiUrl }: any) {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">Test</h1>
-        <p className="text-lg">This is a portfolio page.</p>
+            <h1 className="text-4xl font-bold">Test</h1>
+            <p className="text-lg">This is a portfolio page.</p>
+            <p className="text-lg">API URL: {apiUrl}</p>
         </main>
     );
+}
+
+export async function getServerSideProps() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL_PROD || process.env.NEXT_PUBLIC_API_URL_DEV;
+
+    // Pass the apiUrl to your page as a prop
+    return { props: { apiUrl } };
 }
