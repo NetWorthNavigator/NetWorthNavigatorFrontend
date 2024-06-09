c52ab02262634e3b8d88240fc32527d4e0b0a0c8
Alex Keith <alex.michael.keith@gmail.com>
2024-06-09 12:43:09 -0400

    logging test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Header from "../components/header/Header";
 import { API_URL } from "../constants";
 export default function Home() {
+  console.log('API_URL:', API_URL);
+  console.log('NEXT_PUBLIC_API_URL_PROD:', process.env.NEXT_PUBLIC_API_URL_PROD);
+  console.log('NEXT_PUBLIC_API_URL_DEV:', process.env.NEXT_PUBLIC_API_URL_DEV);
   return (
     <main className="flex h-screen w-screen">
       <Header />
