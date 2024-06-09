137130ea524f069763f52652a7766853e1a7b725
Alex Keith <alex.michael.keith@gmail.com>
2024-06-09 15:08:28 -0400

    test api

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -6,15 +6,11 @@ export default function Portfolio({ apiUrl }: any) {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold">Test</h1>
             <p className="text-lg">This is a portfolio page.</p>
-            <p className="text-lg">API URL: {apiUrl}</p>
             <p className="text-lg">Another API: {API_URL} </p>
         </main>
     );
 }
 
 export async function getServerSideProps() {
-    const apiUrl = API_URL;
-
-    // Pass the apiUrl to your page as a prop
-    return { props: { apiUrl } };
+    return { props: { API_URL: API_URL } };
 }
